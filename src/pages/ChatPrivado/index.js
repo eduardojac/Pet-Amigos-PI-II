e22b77ecbf782cb24e2a337bfe46f1e53fca1360
eduardojac <eduardojacome0129@gmail.com>
2021-11-13 15:50:29 -0300

    Correção bug chat

--- src/pages/ChatPrivado/index.js
+++ src/pages/ChatPrivado/index.js
@@ -12,73 +12,7 @@ import firebase from '../../../firebaseconection';
 import { concat } from 'react-native-reanimated';
 
 export default function ChatPrivado({ route }) {
-  /* const [messages, setMessages] = useState([]);
-   const navigation = useNavigation();
-   async function handlePress() {
-   }
- 
-   const AbrirHome = () => {
-     navigation.reset({
-       routes: [{ name: 'Home' }]
-     })
-   }
- 
-   const user_id = firebase.auth().currentUser.uid
-   const [nome, setNome] = useState('');
-   const [foto, setFoto] = useState(null)
- 
-   firebase.firestore().collection('clientes').where("id", "==", user_id)
-     .get()
-     .then((querySnapshot) => {
-       querySnapshot.forEach((doc) => {
-         // doc.data() is never undefined for query doc snapshots
-         //console.log(doc.id, " => ", doc.data().nome);
-         setNome(doc.data().nome);
-         setFoto(doc.data().foto)
- 
-       });
-     })
-     .catch((error) => {
-       console.log("Error getting documents: ", error);
-     });
- 
-   useEffect(() => {
-     const subscribe = firebase.firestore().collection('chatId').onSnapshot((querySnapshot) => {
-       querySnapshot.docChanges().forEach((change) => {
-         if (change.type === 'added') {
-           let data: any = change.doc.data();
-           data.createdAt = data.createdAt.toDate()
-           setMessages((prevMessage) => GiftedChat.append(prevMessage, data));
-         }
-       })
-     })
- 
-     return () => subscribe()
-   }, [])
- 
-   function onSend(messages: IMessage[]) {
-     firebase.firestore().collection('chatId').doc(Date.now().toString()).set(messages[0])
-   }
- 
-   return (
-     <View style={styles.container}>
-       <View style={styles.botaoMenu}>
-         <TouchableOpacity style={styles.acessoMenu} onPress={AbrirHome}>
-           <Ionicons name="arrow-back-circle-outline" size={50} color="black" style={{ backgroundColor: "#C41D00" }} />
-         </TouchableOpacity>
-         <Text style={{ color: 'white', fontSize: 22, fontWeight: 'bold' }}>Mensagens</Text>
-       </View>
-       <GiftedChat
-         messages={messages}
-         onSend={(messages) => onSend(messages)}
-         showAvatarForEveryMessage={true}
-         user={{
-           _id: user_id,
-         }}
-       />
-     </View>
-   )
- } */
+
   const navigation = useNavigation();
   const AbrirMensagens = () => {
     navigation.reset({
@@ -96,34 +30,6 @@ export default function ChatPrivado({ route }) {
 
   const [messages, setMessages] = useState([]);
 
-  //const [Id, setId] = useState('')
-  //const [IdDoUsuario, setIdDoUsuario] = useState('')
-
-  /*//Exibir lista de usuários
-  firebase.firestore().collection('clientes').where("id", "==", user_id)
-    .get()
-    .then((querySnapshot) => {
-      querySnapshot.forEach((doc) => {
-        data.push({
-          ...doc.data(),
-          key: doc.id
-        })
-        // doc.data() is never undefined for query doc snapshots
-        //console.log(doc.id, " => ", doc.data().nome);
-        setData(data)
-        setNome(doc.data().nome);
-        setAnimacaoNome(false)
-        setFotoUrl(doc.data().foto)
-        setId(doc.id)
- 
-      });
-    })
-    .catch((error) => {
-      console.log("Error getting documents: ", error);
-    }); */
-
-  //Exibir lista de parceiros cadastrados
-
   const ref = firebase.firestore().collection('clientes').where('id', "!=", user_id);
   useEffect(() => {
     ref.onSnapshot(querySnapshot => {
@@ -133,92 +39,34 @@ export default function ChatPrivado({ route }) {
           ...doc.data(),
           key: doc.id
         })
-        // setId(doc.id)
-        //setIdDoUsuario(doc.data().id)
       })
       setData(data)
-
-
     })
     return;
   }, [])
 
-  /*firebase.firestore().collection('clientes').where("id", "!=", user_id)
-    .get()
-    .then((querySnapshot) => {
-      const data = []
-      querySnapshot.forEach((doc) => {
-        // doc.data() is never undefined for query doc snapshots
-        //console.log(doc.id, " => ", doc.data().nome);
-        data.push({
-          ...doc.data(),
-          key: doc.id
+
+  const origemDestino = user_id + route.params?.id
+  const destinoOrigem = route.params?.id + user_id
+
+    useEffect(() => {
+      const subscribe = firebase.firestore().collection('chatId').where('user._id', 'in', [origemDestino, destinoOrigem]).onSnapshot((querySnapshot) => {
+        querySnapshot.docChanges().forEach((change) => {
+          if (change.type === 'added') {
+            let data: any = change.doc.data();
+            data.createdAt = data.createdAt.toDate()
+            setMessages((prevMessage) => GiftedChat.append(prevMessage, data));
+
+          }
         })
-        //setId(doc.id)
-        //setIdDoUsuario(doc.data().id)
-      });
-      setData(data) 'user.destino', '==', route.params?.id
-    })
-    .catch((error) => {
-      console.log("Error getting documents: ", error);
-    });  */
-
-  //.where('user._id','==', user_id).where('user.destino', '==', route.params?.id)  É TIPO UMA DESSA: where('user._id','==', user_id || 'user._id', '==', route.params?.id).where('destino', '==', route.params?.id)
-
-  /*const adicionarDestino = () => {
-    firebase.firestore().collection('chatId').add({destino: route.params?.id});
-  }*/
-
-  /* async function filtros() {
-     const origem = firebase.firestore().collection('chatId').where('user._id', 'in', [user_id,route.params?.id]).
-     const destino = firebase.firestore().collection('chatId').where('destino', 'in', [user_id,route.params?.id])
-
-     const [
-       origemSnapshot,
-       destinoSnapshot
-     ] = await Promise.all([origem,destino])
-
-     const eOrigem = origemSnapshot.docs
-     const eDestino = destinoSnapshot.docs
-     
-     return _.concat(eOrigem,eDestino)
-
-   } */
-
-  useEffect(() => {
-    const subscribe = firebase.firestore().collection('chatId').where('user.destino', 'in', [user_id, route.params?.id]).where('user._id', '==', user_id).onSnapshot((querySnapshot) => {
-      querySnapshot.docChanges().forEach((change) => {
-        if (change.type === 'added') {
-          let data: any = change.doc.data();
-          data.createdAt = data.createdAt.toDate()
-          setMessages((prevMessage) => GiftedChat.append(prevMessage, data));
-
-        }
       })
-    })
-    return () => subscribe()
-  }, [])
-
-  useEffect(() => {
-    const subscribe2 = firebase.firestore().collection('chatId').where('user.destino', 'in', [user_id, route.params?.id]).where('user._id', '==', route.params?.id).onSnapshot((querySnapshot) => {
-      querySnapshot.docChanges().forEach((change) => {
-        if (change.type === 'added') {
-          let data: any = change.doc.data();
-          data.createdAt = data.createdAt.toDate()
-          setMessages((prevMessage) => GiftedChat.append(prevMessage, data));
-
-        }
-      })
-    })
-    return () => subscribe2()
-  }, [])
-
+      return () => subscribe()
+    }, [])
 
   function onSend(messages: IMessage[]) {
     firebase.firestore().collection('chatId').doc(Date.now().toString()).set(messages[0])
 
   }
-
 
   return (
     <View style={styles.container}>
@@ -234,8 +82,7 @@ export default function ChatPrivado({ route }) {
         messages={messages}
         onSend={(messages) => onSend(messages)}
         user={{
-          _id: user_id,
-          destino: route.params?.id
+          _id: user_id + route.params?.id
         }}
 
       />
